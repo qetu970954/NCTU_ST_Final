8602ad6a09ae058a91719aaf2b1d1c89767d4141
Tzu-Yee <fdja1974.cs07g@nctu.edu.tw>
2019-06-12 11:37:31 +0800

    Add test for high coverage, modify function signature for clarity.

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -29,35 +29,35 @@ def walker():
 #     'top node' is the common ancestor
 #     'downwards' is a list of node
 # We will test whether the return value of a walk() is identical to the expectation
-def test_walk_upwards(tree, walker):
+def test_walking_upwards(tree, walker):
     # start from 'c', end at 'b'
     actual = walker.walk(tree["c"], tree["b"])
     expect = ((tree["c"], tree["d"],), tree["b"], ())
     assert actual == expect, "Incorrect upward path."
 
 
-def test_walk_upwards2(tree, walker):
+def test_walking_upwards2(tree, walker):
     # start from 'h', end at 'r'
     actual = walker.walk(tree["h"], tree["r"])
     expect = ((tree["h"], tree["i"], tree["g"],), tree["r"], ())
     assert actual == expect, "Incorrect upward path."
 
 
-def test_walk_downwards(tree, walker):
+def test_walking_downwards(tree, walker):
     # start from 'b', end at 'c'
     actual = walker.walk(tree["b"], tree["c"])
     expect = ((), tree["b"], (tree["d"], tree["c"],))
     assert actual == expect, "Incorrect downward path."
 
 
-def test_walk_downwards2(tree, walker):
+def test_walking_downwards2(tree, walker):
     # start from 'r', end at 'e'
     actual = walker.walk(tree["r"], tree["e"])
     expect = ((), tree["r"], (tree["b"], tree["d"], tree["e"],))
     assert actual == expect, "Incorrect downward path."
 
 
-def test_walk_from_Root_to_Root(tree, walker):
+def test_walking_from_root_to_root(tree, walker):
     # start from 'r', end at 'r'
     actual = walker.walk(tree["r"], tree["r"])
     expect = ((), tree["r"], ())
@@ -72,11 +72,16 @@ def test_walk_from_Root_to_Root(tree, walker):
         ("b", "g", (("b",), "r", ("g",))),
     ]
 )
-def test_walk_general_case(tree, walker, source, destination, expectation):
+def test_general_walking(tree, walker, source, destination, expectation):
     def enrich_expectation(a_tree, expect) -> tuple:
-        # I don't know how to use "tree" in the parameterized example.
-        # As a workaround, this function enriches the expectation by invoke tree on expectation.
+        # enriches the expectation by invoke tree on expectation.
         return tuple(a_tree[i] for i in expect[0]), a_tree[expect[1]], tuple(a_tree[i] for i in expect[2])
 
     actual = walker.walk(tree[source], tree[destination])
     assert actual == enrich_expectation(tree, expectation), "Incorrect walk path from a child to another child."
+
+
+def test_walking_to_different_tree(tree, walker):
+    from anytree import WalkError
+    with pytest.raises(WalkError):
+        walker.walk(tree["r"], Node("r"))
